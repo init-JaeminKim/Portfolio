4ebd8c24c9487241057b15618989a9f51057332b
Jaemin Kim <jkim37@g.emporia.edu>
2020-12-17 20:27:43 -0600

    imposter

--- js/amongUs.js
+++ js/amongUs.js
@@ -1,27 +1,27 @@
 var canvas = document.getElementById('amongUs');
-var ctx_amongUs = canvas.getContext('2d');
-var canvasInterval = requestAnimationFrame(canvasAnimation_amongUs);
-var circleX_amongUs = 0;
-var circleY_amongUs = canvas.height / 2;
-var amplitude_amongUs = 30;
-var frequency_amongUs = 15;
+var ctx_amongUS = canvas.getContext('2d');
+var canvasInterval = requestAnimationFrame(canvasAnimation_amongUS);
 
-var mBall_X = 0;
-var mBall_Y = 0;
+var canvas_voted = document.getElementById('voted');
+var ctx_voted = canvas_voted.getContext('2d');
 
-var rare_amongUs = [];
-var middle_amongUs = [];
-var front_amongUs = [];
+var rare_amongUS = [];
+var middle_amongUS = [];
+var front_amongUS = [];
+
+var imposterX_amongUS = 0;
+var imposterY_amongUS = canvas.height / 2;
+var image_amongUS = document.getElementById('imposter')
 
 
-function generateStar_amongUs() {
+function generateStar_amongUS() {
 
 
     for (var i = 0; i < 10; i++) {
         var x = Math.floor(Math.random() * -200);
         var y = Math.floor(Math.random() * 200);
 
-        rare_amongUs.push([x, y]);
+        rare_amongUS.push([x, y]);
 
     }
 
@@ -29,76 +29,94 @@ function generateStar_amongUs() {
         var x = Math.floor(Math.random() * -200);
         var y = Math.floor(Math.random() * 200);
 
-        middle_amongUs.push([x, y]);
+        middle_amongUS.push([x, y]);
     }
 
     for (var i = 0; i < 7; i++) {
         var x = Math.floor(Math.random() * -200);
         var y = Math.floor(Math.random() * 200);
 
-        front_amongUs.push([x, y]);
+        front_amongUS.push([x, y]);
     }
 
 }
 
-function canvasAnimation_amongUs() {
+function canvasAnimation_amongUS() {
 
-    drawRare_amongUs();
-    drawMiddle_amongUs();
-    drawFront_amongUs();
+    imposterX_amongUS++;
 
-    canvasInterval = requestAnimationFrame(canvasAnimation_amongUs);
+    drawRare_amongUS();
+    drawMiddle_amongUS();
+    drawFront_amongUS();
+    imposter_amongUS(imposterX_amongUS, imposterY_amongUS);
+
+    canvasInterval = requestAnimationFrame(canvasAnimation_amongUS);
 
 
 }
 
-function drawRare_amongUs() {
-    var r_amongUs;
-    clearCanvas_amongUs();
-    ctx_amongUs.fillStyle = "rgba(255,255,255, 0.5)";
-    ctx_amongUs.beginPath();
-    for (var i = 0; i < rare_amongUs.length; i++) {
-        r_amongUs = (Math.random() * 0.8) + 0.6;
-        ctx_amongUs.moveTo(rare_amongUs[i][0], rare_amongUs[i][1]);
-        ctx_amongUs.arc(rare_amongUs[i][0] += 0.5, rare_amongUs[i][1], 2, 0, Math.PI * 2, true);
+function drawRare_amongUS() {
+    var r_amongUS;
+    clearCanvas_amongUS();
+    ctx_amongUS.fillStyle = "rgba(255,255,255, 0.5)";
+    ctx_amongUS.beginPath();
+    for (var i = 0; i < rare_amongUS.length; i++) {
+        r_amongUS = (Math.random() * 0.8) + 0.6;
+        ctx_amongUS.moveTo(rare_amongUS[i][0], rare_amongUS[i][1]);
+        ctx_amongUS.arc(rare_amongUS[i][0] += 0.5, rare_amongUS[i][1], 2, 0, Math.PI * 2, true);
     }
-    ctx_amongUs.fill();
+    ctx_amongUS.fill();
 
 }
 
-function drawMiddle_amongUs() {
-    var r_amongUs;
-    ctx_amongUs.fillStyle = "rgba(255,255,255, 0.7)";
-    ctx_amongUs.beginPath();
-    for (var i = 0; i < middle_amongUs.length; i++) {
-        r_amongUs = (Math.random() * 1) + 0.8;
-        ctx_amongUs.moveTo(middle_amongUs[i][0], middle_amongUs[i][1]);
-        ctx_amongUs.arc(middle_amongUs[i][0] += 0.7, middle_amongUs[i][1], 2.5, 0, Math.PI * 2, true);
+function drawMiddle_amongUS() {
+    var r_amongUS;
+    ctx_amongUS.fillStyle = "rgba(255,255,255, 0.7)";
+    ctx_amongUS.beginPath();
+    for (var i = 0; i < middle_amongUS.length; i++) {
+        r_amongUS = (Math.random() * 1) + 0.8;
+        ctx_amongUS.moveTo(middle_amongUS[i][0], middle_amongUS[i][1]);
+        ctx_amongUS.arc(middle_amongUS[i][0] += 0.7, middle_amongUS[i][1], 2.5, 0, Math.PI * 2, true);
     }
-    ctx_amongUs.fill();
+    ctx_amongUS.fill();
 
 }
 
-function drawFront_amongUs() {
-    var r_amongUs;
-    ctx_amongUs.fillStyle = "rgba(255,255,255, 1)";
-    ctx_amongUs.beginPath();
-    for (var i = 0; i < front_amongUs.length; i++) {
-        r_amongUs = (Math.random() * 3) + 1;
-        ctx_amongUs.moveTo(front_amongUs[i][0], front_amongUs[i][1]);
-        ctx_amongUs.arc(front_amongUs[i][0] += 1, front_amongUs[i][1], 3, 0, Math.PI * 2, true);
+function drawFront_amongUS() {
+    var r_amongUS;
+    ctx_amongUS.fillStyle = "rgba(255,255,255, 1)";
+    ctx_amongUS.beginPath();
+    for (var i = 0; i < front_amongUS.length; i++) {
+        r_amongUS = (Math.random() * 3) + 1;
+        ctx_amongUS.moveTo(front_amongUS[i][0], front_amongUS[i][1]);
+        ctx_amongUS.arc(front_amongUS[i][0] += 1, front_amongUS[i][1], 3, 0, Math.PI * 2, true);
     }
-    ctx_amongUs.fill();
+    ctx_amongUS.fill();
 
-    ctx_amongUs.font = "35px Arial";
-    ctx_amongUs.fillText("Jaemin is a developer", 330, 100);
+    ctx_amongUS.font = "35px Arial";
+    ctx_amongUS.fillText("Jaemin is a developer", 330, 100);
+
+
+
 }
 
-function clearCanvas_amongUs() {
-    ctx_amongUs.clearRect(0, 0, canvas.width, canvas.height);
+function imposter_amongUS(x, y) {
+
+    ctx_voted.clearRect(0, 0, canvas.width, canvas.height);
+    ctx_voted.drawImage(image_amongUS, x, y / 2, 70, 70)
+
+    if (imposterX_amongUS > canvas.width * 1.1) {
+        imposterX_amongUS = 0;
+    }
+
+}
+
+function clearCanvas_amongUS() {
+    ctx_amongUS.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 setInterval(() => {
-    generateStar_amongUs()
+    generateStar_amongUS()
 }, 4000);
-drawRare_amongUs();
+
+drawRare_amongUS();
